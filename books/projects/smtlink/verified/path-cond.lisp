c786e42ff0b93a5e250636885f277ee88c588523
Yan Peng <pengyan2009@gmail.com>
2020-04-24 14:32:52 -0700

    Updated

--- books/projects/smtlink/verified/path-cond.lisp
+++ books/projects/smtlink/verified/path-cond.lisp
@@ -21,75 +21,6 @@
 ;; -------------------------------------------------------------------
 ;;        Functions about path-cond
 
-(define path-test ((path-cond pseudo-termp)
-                   (expr pseudo-termp)
-                   state)
-  :returns (ok booleanp)
-  (b* ((path-cond (pseudo-term-fix path-cond))
-       (expr (pseudo-term-fix expr))
-       (substed-cond (term-substitution path-cond `((,expr . 'nil)) t))
-       ((mv err eval-cond) (partial-eval substed-cond nil state))
-       ((if err) nil)
-       ((unless eval-cond) t))
-    nil))
-
-(define path-test-list ((path-cond pseudo-termp)
-                        (expr-conj pseudo-termp)
-                        state)
-  :returns (ok booleanp)
-  :measure (acl2-count (pseudo-term-fix expr-conj))
-  (b* ((path-cond (pseudo-term-fix path-cond))
-       (expr-conj (pseudo-term-fix expr-conj))
-       ((unless (is-conjunct? expr-conj))
-        (path-test path-cond expr-conj state))
-       ((if (equal expr-conj ''t)) t)
-       ((list & expr-hd expr-tl &) expr-conj)
-       (yes? (path-test path-cond expr-hd state))
-       ((if yes?) (path-test-list path-cond expr-tl state)))
-    nil))
-
-#|
-(path-test-list '(if (integerp x) (if (null x) 't 'nil) 'nil)
-                '(if (integerp x) 't 'nil)
-                state)
-(path-test-list '(if (integerp x) (if (null x) 't 'nil) 'nil)
-                '(if (rationalp x) 't 'nil)
-                state)
-(path-test-list '(if (integerp x) (if (null x) 't 'nil) 'nil)
-                '(if x 't 'nil)
-                state)
-(path-test-list '(if (if (integerp x)
-                         (if (null x) 't 'nil) 'nil)
-                     (rationalp x)
-                   'nil)
-                '(if x (if (integerp x) 't 'nil) 'nil)
-                state)
-(path-test-list '(IF (IF (RATIONALP R1) 'T 'NIL)
-                     (IF (IF (RATIONAL-INTEGER-ALISTP AL)
-                             'T
-                             'NIL)
-                         'T
-                         'NIL)
-                     'NIL)
-                '(rational-integer-alistp al)
-                state)
-|#
-
-(define path-test-list-or ((path-cond pseudo-termp)
-                           (expr-conj pseudo-termp)
-                           state)
-  :returns (ok booleanp)
-  :measure (acl2-count (pseudo-term-fix expr-conj))
-  (b* ((path-cond (pseudo-term-fix path-cond))
-       (expr-conj (pseudo-term-fix expr-conj))
-       ((unless (is-conjunct? expr-conj))
-        (path-test path-cond expr-conj state))
-       ((if (equal expr-conj ''t)) nil)
-       ((list & expr-hd expr-tl &) expr-conj)
-       (yes? (path-test path-cond expr-hd state))
-       ((if yes?) t))
-    (path-test-list-or path-cond expr-tl state)))
-
 ;; look-up-path-cond will ignore the whole branch of or's and only look for top
 ;; level and's
 (define look-up-path-cond-acc ((term pseudo-termp)
@@ -112,23 +43,42 @@
 
 (verify-guards look-up-path-cond-acc)
 
+(defthm correctness-of-lookup-path-cond-acc
+   (implies (and (ev-smtcp-meta-extract-global-facts)
+                 (pseudo-termp term)
+                 (pseudo-termp path-cond)
+                 (pseudo-termp acc)
+                 (type-to-types-alist-p supertype-alst)
+                 (alistp a)
+                 (ev-smtcp path-cond a)
+                 (ev-smtcp acc a))
+            (ev-smtcp (look-up-path-cond-acc term path-cond supertype-alst acc)
+                      a))
+   :hints (("Goal"
+            :in-theory (e/d (look-up-path-cond-acc is-conjunct?)
+                            (implies-of-is-conjunct?
+                             member-equal symbol-listp
+                             consp-of-is-conjunct?
+                             pseudo-term-listp-of-symbol-listp
+                             default-cdr)))))
+
 (define look-up-path-cond ((term pseudo-termp)
                            (path-cond pseudo-termp)
                            (supertype-alst type-to-types-alist-p))
   :returns (type-term pseudo-termp)
-  (look-up-path-cond-acc term path-cond supertype-alst ''t))
-
-(skip-proofs
-(defthm correctness-of-lookup-path-cond
-  (implies (and (ev-smtcp-meta-extract-global-facts)
-                (pseudo-termp term)
-                (pseudo-termp path-cond)
-                (type-to-types-alist-p supertype-alst)
-                (alistp a)
-                (ev-smtcp path-cond a))
-           (ev-smtcp (look-up-path-cond term path-cond supertype-alst)
-                     a)))
-)
+  (look-up-path-cond-acc term path-cond supertype-alst ''t)
+  ///
+  (defthm correctness-of-lookup-path-cond
+    (implies (and (ev-smtcp-meta-extract-global-facts)
+                  (pseudo-termp term)
+                  (pseudo-termp path-cond)
+                  (type-to-types-alist-p supertype-alst)
+                  (alistp a)
+                  (ev-smtcp path-cond a))
+             (ev-smtcp (look-up-path-cond term path-cond supertype-alst)
+                       a))
+    :hints (("Goal"
+             :expand (look-up-path-cond term path-cond supertype-alst)))))
 
 #|
 (look-up-path-cond 'r1
@@ -187,6 +137,111 @@
                      (maybe-integerp)))
 |#
 
+(define path-test ((path-cond pseudo-termp)
+                   (expr pseudo-termp)
+                   state)
+  :returns (ok booleanp)
+  :measure (acl2-count (pseudo-term-fix path-cond))
+  (b* ((expr (pseudo-term-fix expr))
+       (path-cond (pseudo-term-fix path-cond))
+       ((if (equal path-cond ''t)) nil)
+       ((if (equal path-cond expr)) t)
+       ((unless (is-conjunct? path-cond)) nil)
+       ((list* & path-hd path-tl &) path-cond))
+    (or (path-test path-hd expr state)
+        (path-test path-tl expr state)))
+  ///
+  (defthm correctness-of-path-test
+    (implies (and (ev-smtcp-meta-extract-global-facts)
+                  (pseudo-termp expr)
+                  (pseudo-termp path-cond)
+                  (alistp a)
+                  (ev-smtcp path-cond a)
+                  (path-test path-cond expr state))
+             (ev-smtcp expr a))
+    :hints (("Goal"
+             :in-theory (e/d (path-test is-conjunct?)
+                             (implies-of-is-conjunct?
+                              consp-of-is-conjunct?
+                              symbol-listp
+                              pseudo-term-listp-of-symbol-listp
+                              acl2::symbol-listp-of-cdr-when-symbol-listp
+                              default-car default-cdr))))))
+
+(define path-test-list ((path-cond pseudo-termp)
+                        (expr-conj pseudo-termp)
+                        state)
+  :returns (ok booleanp)
+  :measure (acl2-count (pseudo-term-fix expr-conj))
+  (b* ((path-cond (pseudo-term-fix path-cond))
+       (expr-conj (pseudo-term-fix expr-conj))
+       ((unless (is-conjunct? expr-conj))
+        (path-test path-cond expr-conj state))
+       ((if (equal expr-conj ''t)) t)
+       ((list & expr-hd expr-tl &) expr-conj)
+       (yes? (path-test path-cond expr-hd state))
+       ((if yes?) (path-test-list path-cond expr-tl state)))
+    nil))
+
+(defthm correctness-of-path-test-list
+  (implies (and (ev-smtcp-meta-extract-global-facts)
+                (pseudo-termp expr-conj)
+                (pseudo-termp path-cond)
+                (alistp a)
+                (ev-smtcp path-cond a)
+                (path-test-list path-cond expr-conj state))
+           (ev-smtcp expr-conj a))
+  :hints (("Goal"
+           :in-theory (e/d (path-test-list is-conjunct?)
+                           (implies-of-is-conjunct?
+                            consp-of-is-conjunct?
+                            symbol-listp
+                            pseudo-term-listp-of-symbol-listp
+                            acl2::symbol-listp-of-cdr-when-symbol-listp
+                            default-car default-cdr)))))
+
+#|
+(path-test-list '(if (integerp x) (if (null x) 't 'nil) 'nil)
+                '(if (integerp x) 't 'nil)
+                state)
+(path-test-list '(if (integerp x) (if (null x) 't 'nil) 'nil)
+                '(if (rationalp x) 't 'nil)
+                state)
+(path-test-list '(if (integerp x) (if (null x) 't 'nil) 'nil)
+                '(if x 't 'nil)
+                state)
+(path-test-list '(if (if (integerp x)
+                         (if (null x) 't 'nil) 'nil)
+                     (rationalp x)
+                   'nil)
+                '(if x (if (integerp x) 't 'nil) 'nil)
+                state)
+(path-test-list '(IF (IF (RATIONALP R1) 'T 'NIL)
+                     (IF (IF (RATIONAL-INTEGER-ALISTP AL)
+                             'T
+                             'NIL)
+                         'T
+                         'NIL)
+                     'NIL)
+                '(rational-integer-alistp al)
+                state)
+|#
+
+(define path-test-list-or ((path-cond pseudo-termp)
+                           (expr-conj pseudo-termp)
+                           state)
+  :returns (ok booleanp)
+  :measure (acl2-count (pseudo-term-fix expr-conj))
+  (b* ((path-cond (pseudo-term-fix path-cond))
+       (expr-conj (pseudo-term-fix expr-conj))
+       ((unless (is-conjunct? expr-conj))
+        (path-test path-cond expr-conj state))
+       ((if (equal expr-conj ''t)) nil)
+       ((list & expr-hd expr-tl &) expr-conj)
+       (yes? (path-test path-cond expr-hd state))
+       ((if yes?) t))
+    (path-test-list-or path-cond expr-tl state)))
+
 ;; shadow-path-cond-acc will shadow the whole or branch if it contains
 ;; variables shadowed by formals.
 (define shadow-path-cond-acc ((formals symbol-listp)
@@ -210,20 +265,36 @@
 
 (verify-guards shadow-path-cond-acc)
 
+(defthm correctness-of-shadow-path-cond-acc
+  (implies (and (ev-smtcp-meta-extract-global-facts)
+                (symbol-listp formals)
+                (pseudo-termp path-cond)
+                (pseudo-termp acc)
+                (alistp a)
+                (ev-smtcp path-cond a)
+                (ev-smtcp acc a))
+           (ev-smtcp (shadow-path-cond-acc formals path-cond acc) a))
+  :hints (("Goal"
+           :in-theory (e/d (shadow-path-cond-acc is-conjunct?)
+                           (symbol-listp default-car default-cdr
+                                         consp-of-is-conjunct?
+                                         pseudo-term-listp-of-symbol-listp)))))
+
 (define shadow-path-cond ((formals symbol-listp)
                           (path-cond pseudo-termp))
   :returns (shadowed-path-cond pseudo-termp)
-  (shadow-path-cond-acc formals path-cond ''t))
-
-(skip-proofs
- (defthm correctness-of-shadow-path-cond
-   (implies (and (ev-smtcp-meta-extract-global-facts)
-                 (symbol-listp formals)
-                 (pseudo-termp path-cond)
-                 (alistp a)
-                 (ev-smtcp path-cond a))
-            (ev-smtcp (shadow-path-cond formals path-cond) a)))
- )
+  (shadow-path-cond-acc formals path-cond ''t)
+  ///
+  (defthm correctness-of-shadow-path-cond
+    (implies (and (ev-smtcp-meta-extract-global-facts)
+                  (symbol-listp formals)
+                  (pseudo-termp path-cond)
+                  (alistp a)
+                  (ev-smtcp path-cond a))
+             (ev-smtcp (shadow-path-cond formals path-cond) a))
+    :hints (("Goal"
+             :in-theory (enable shadow-path-cond))))
+)
 
 #|
 (shadow-path-cond '(x)
